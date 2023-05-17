73586077aeeac3437b60835ed9e77e5fe21eb8a3
Om Patel <122214228+Omi-Patel@users.noreply.github.com>
2023-05-17 12:31:31 +0530

    patterns

---
+++ Pattern/Pattern4.java
@@ -0,0 +1,48 @@
+class Pattern4{
+	public static void main(String[] args){
+		int n = 7;
+		
+		for(int i=1; i<=n; i++){
+			for(int j=1; j<=n; j++){
+				if(i == 1){
+					
+					if(j <= n/2+1 || j == n){
+						System.out.print("*");
+					}else{
+						System.out.print(" ");
+					}
+					
+				}else if(i <= n/2){
+					
+					if(j == n/2+1 || j == n){
+						System.out.print("*");
+					}else{
+						System.out.print(" ");
+					}
+					
+				}else if(i == n/2+1){
+					
+					System.out.print("*");
+					
+				}else if(i < n){
+					
+					if(j == n/2+1 || j == 1){
+						System.out.print("*");
+					}else{
+						System.out.print(" ");
+					}
+					
+				}else{
+					
+					if(j >= n/2+1 || j == 1){
+						System.out.print("*");
+					}else{
+						System.out.print(" ");
+					}
+					
+				}
+			}
+			System.out.println();
+		}
+	}
+}
